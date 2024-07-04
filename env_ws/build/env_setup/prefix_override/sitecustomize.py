b34eae71d453c3a6fef2a8aa94a3ae87a4c7932e
Sukruthi-C <sukruthi.2402@gmail.com>
2024-07-04 14:17:27 -0400

    removed YOLO object detection codebase and using ultralytics for YOLO

--- env_ws/build/env_setup/prefix_override/sitecustomize.py
+++ env_ws/build/env_setup/prefix_override/sitecustomize.py
@@ -1,3 +1,4 @@
 import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/sukruthi/Desktop/Start-up/Developer/ObjectTrackingUsingEyeGaze/env_ws/install/env_setup'
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/sukruthi/Desktop/Start-up/Developer/ObjectTrackingUsingEyeGaze/env_ws/install/env_setup'
--- env_ws/src/env_setup/env_setup/intel_sub.py
+++ env_ws/src/env_setup/env_setup/intel_sub.py
@@ -1,8 +1,16 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 import cv2
+import os
+from PIL import Image as PILImage
+from io import BytesIO
+
+from ultralytics import YOLO
+
+# load the model
+model = YOLO("yolov8n.pt")
 
 
 class IntelSubscriber(Node):
@@ -10,13 +18,63 @@ class IntelSubscriber(Node):
         super().__init__("intel_subscriber")
         self.subscription_rgb = self.create_subscription(Image,"rgb_frame",self.rgb_frame_callback,10)
         self.br_rgb = CvBridge()
+        # self.image_directory = '/ObjectTrackingUsingEyeGaze/YOLO-Object-Detection/darknet/data'
+        self.timer = self.create_timer(10,self.timerCallback)
+        # self.image_captured = False
 
-    def rgb_frame_callback(self,data):
+    def rgb_frame_callback(self, data):
         self.get_logger().warning("Receiving RGB frame")
-        current_frame = self.br_rgb.imgmsg_to_cv2(data)
-        cv2.imshow("RGB",current_frame)
+        
+        try:
+            # Check the encoding of the incoming image
+            if data.encoding == "bgr8" or data.encoding == "rgb8":
+                current_frame = self.br_rgb.imgmsg_to_cv2(data, "bgr8")
+            elif data.encoding == "8UC3":
+                current_frame = self.br_rgb.imgmsg_to_cv2(data)
+            else:
+                self.get_logger().error(f"Unsupported encoding: {data.encoding}")
+                return
+
+        except CvBridgeError as e:
+            self.get_logger().error(f"Failed to convert image: {str(e)}")
+            return
+        rgb_frame = cv2.cvtColor(current_frame,cv2.COLOR_BGR2RGB)
+        pil_img = PILImage.fromarray(rgb_frame)
+
+        # run inference
+        results = model([pil_img])
+        
+
+        # object detection
+        # results = model(current_frame,stream=True)
+        for i,result in enumerate (results):
+            im_bgr = result.plot()
+            im_rgb = PILImage.fromarray(im_bgr[...,::-1])
+            result.show()
+            annotated_frame = cv2.cvtColor(im_bgr,cv2.COLOR_RGB2BGR)
+
+            cv2.imshow("Detected objects", annotated_frame)
+            cv2.waitKey(1)
+
+        self.destroy_timer(self.timer)
         cv2.waitKey(1)
 
+
+    def timerCallback(self):
+        self.get_logger().info("10 seconds passed, capturing an image.")
+
+    # def detection_callback(self,current_frame):
+    #     # run inference on the source
+    #     results = model(source=current_frame,conf=0.4,save=True)
+    #     return results
+
+# you need to write a class to send images to yolo directly
+# see what yolo outputs
+
+# class YOLO_detection():
+
+#     def __init__(self):
+#         self.get_logger().info
 
 def main():
     rclpy.init()
